5e61f47ccb1c311bb0128a3926fa4f60db0cf379
X-Y-Zhou <2827644213@qq.com>
2023-10-08 22:50:26 +0800

    update

--- Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/Infer_attribute_1.jl
+++ Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/Infer_attribute_1.jl
@@ -1,7 +1,7 @@
 using Flux, DiffEqSensitivity, DifferentialEquations
 using Distributions, Distances
 using DelimitedFiles, Plots,CSV,DataFrames
-using BlackBoxOptim
+using BlackBoxOptim,OptimalTransport
 
 include("../../../utils.jl")
 
@@ -9,9 +9,6 @@ a = 0.0282
 b = 3.46
 N = 81
 τ = 120
-
-# Simulation data
-# SSA_data = readdlm("Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/Inference_data/set1/30-210_1.csv",',')[2:end,:]
 
 # model initialization
 latent_size = 10;
@@ -38,10 +35,9 @@ function f_Extenicity!(x,p1,p2,a,b,Attribute,ϵ)
 end
 
 using CSV,DataFrames
-df = CSV.read("Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/params_ct2.csv",DataFrame)
+df = CSV.read("Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/params_tfo.csv",DataFrame)
 params1 = df.params1
-params2 = df.params2[1:length(params2)]
-ps = Flux.params(params1,params2);
+params2 = df.params2[1:length_2]
 
 P_0_distribution_Extenicity = NegativeBinomial(a*τ, 1/(1+b));
 P_0_Extenicity = [pdf(P_0_distribution_Extenicity,i) for i=0:N-1]
@@ -51,67 +47,71 @@ Attribute = 0
 ϵ = zeros(latent_size)
 solution = sol_Extenicity(params1,params2,a,b,Attribute,ϵ,P_0_Extenicity)
 
-sample_size = 5000
-solution = set_one(solution)
-log_value = log.(solution)
+# sample_size = 5000
+# solution = set_one(solution)
+# log_value = log.(solution)
 
-# SSA data
-result_list = []
-set = 1
-width = "2-60"
-
-for dataset = 1:5
-print(dataset,"\n")
-SSA_data = readdlm("Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/Inference_data/set$set/$(width)_$dataset.csv",',')[2:end,:]
-SSA_timepoints = round.(Int, vec(SSA_data).*sample_size)
-
-# Ex = P2mean(SSA_data)
-# Dx = P2var(SSA_data)
-
-# a = 2Ex^2/(Dx-Ex)τ
-# b = (Dx-Ex)/2Ex
-
-function LogLikelihood(kinetic_params)
-    a = 0.0282
-    b = 3.46
+function Objective_func(kinetic_params)
+    a = 0.0232
+    b = 2.96
     Attribute = kinetic_params[1]
-
-    df = CSV.read("Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/params_ct2.csv",DataFrame)
-    params1 = df.params1
-    params2 = df.params2[1:length_2]
 
     ϵ = zeros(latent_size)
     P_0_distribution_Extenicity = NegativeBinomial(a*τ, 1/(1+b));
     P_0_Extenicity = [pdf(P_0_distribution_Extenicity,i) for i=0:N-1]
     solution = sol_Extenicity(params1,params2,a,b,Attribute,ϵ,P_0_Extenicity)
 
-    # loglikelihood_value = Flux.mse(solution,SSA_data[:,i])
+    # solution = set_one(solution)
+    # SSA_data1 = vec(Float64.(SSA_data))
+    # μ = Categorical(vcat(SSA_data1,1-sum(SSA_data1)))
+    # ν = Categorical(vcat(solution,1-sum(solution)))
+    # Objective_value = ot_cost(sqeuclidean, μ, ν)
 
-    solution = set_one(solution)
-    log_value = log.(solution)
-    loglikelihood_value = -sum(SSA_timepoints.*log_value)/sample_size
+    Objective_value = Flux.mse(solution,SSA_data)
 
-    return loglikelihood_value
+    # solution = set_one(solution)
+    # log_value = log.(solution)
+    # loglikelihood_value = -sum(SSA_timepoints.*log_value)/sample_size
+
+    return Objective_value
 end
 
-# LogLikelihood(kinetic_params0)
+# SSA data
+result_list = []
+set = 4
+width = "2-60"
 
-kinetic_params0 = [0.8]
+SSA_data = readdlm("Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/data/set$set/$(width).csv",',')[2:end,:]
+
+@time for dataset = 1:5
+print(dataset,"\n")
+# SSA_data = readdlm("Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/Inference_data/set$set/$(width)_$dataset.csv",',')[2:end,:]
+SSA_data = readdlm("Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/data/set$set/$(width).csv",',')[2:end,:]
+# SSA_timepoints = round.(Int, vec(SSA_data).*sample_size)
+
+# Ex = P2mean(SSA_data)
+# Dx = P2var(SSA_data)
+
+# a_0 = 2Ex^2/(Dx-Ex)τ
+# b_0 = (Dx-Ex)/2Ex
+
+kinetic_params0 = [0.7962]
 SRange = [(0,1)]
-res = bboptimize(LogLikelihood,kinetic_params0 ; Method = :adaptive_de_rand_1_bin_radiuslimited, 
-SearchRange = SRange, NumDimensions = 1, MaxSteps = 150) #参数推断求解
+res = bboptimize(Objective_func,kinetic_params0; Method = :adaptive_de_rand_1_bin_radiuslimited, 
+SearchRange = SRange, NumDimensions = 1, MaxSteps = 400) #参数推断求解
 thetax = best_candidate(res) #优化器求解参数
-# best_fitness(res)
 
-α = 0.0282
-β = 3.46
+α = 0.0232
+β = 2.96
 Attribute = thetax[1]
+
 x1 = Int(round(exp((1-Attribute)*log(30)),digits=0))
 x2 = τ/x1
 distribution = Erlang(x1,x2)
 variance = var(distribution)
+bestfitness = best_fitness(res)
 
-push!(result_list,[α,β,Attribute,x1,x2,variance])
+push!(result_list,[α,β,Attribute,x1,x2,variance,bestfitness])
 end
 
 result_list
@@ -129,9 +129,8 @@ function check_inference(kinetic_params)
     a = kinetic_params[1]
     b = kinetic_params[2]
     Attribute = kinetic_params[3]
-    # Attribute = 0.75
 
-    df = CSV.read("Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/params_ct2.csv",DataFrame)
+    df = CSV.read("Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/params_tfo.csv",DataFrame)
     params1 = df.params1
     params2 = df.params2[1:length_2]
 
@@ -142,36 +141,20 @@ function check_inference(kinetic_params)
     return solution
 end
 
+using Flux
 set
 width
 result_list
 
-dataset = 1
+dataset = 2
 result_list[dataset]
 solution_inference = check_inference(result_list[dataset])
-solution_theoty = check_inference([0.0232,2.96,0.75])
 SSA_data = vec(readdlm("Bursty/Control_rate_Inference/Control_tau_fixed_otherrand_erlang/data/set$set/$(width).csv",',')[2:end,:])
-
-using Flux
 Flux.mse(solution_inference,SSA_data)
-Flux.mse(solution_theoty,SSA_data)
 
 plot(0:N-1,solution_inference,lw=3,label="inference")
-plot!(0:N-1,solution_theoty,lw=3,label="theory",line=:dash)
 plot!(0:N-1,SSA_data,lw=3,label="SSA",line=:dash)
 
-Attribute = 0.1
-x1 = exp((1-Attribute)*log(30))
-x2 = τ/x1
-
-Int(x1)
-Int(round(x1,digits=0))
-
-
-x_temp = [1,2,5,10,20,30]
-x_temp_log = log.(x_temp)
-y_temp = [1,0.8,0.4,0.2,0.1,0]
-y_temp_theory = (-1/log(30)).*(log.(x_temp)).+1
 
 # Erlang(a,b)
 # a = 30;b = 4   # var = 480   [0]    [0]
@@ -180,12 +163,5 @@ y_temp_theory = (-1/log(30)).*(log.(x_temp)).+1
 # a = 5; b = 24  # var = 2880  [0.4]  [0.5268]
 # a = 2; b = 60  # var = 7200  [0.8]  [0.7962]
 # a = 1; b = 120 # var = 14400 [1]    [1]
-
-Attribute_list1 = [1,0.715,0.412,0.128,0]
-Attribute_list2 = [1,0.75,0.5,0.25,0]
-τ_list = [0,30,60,90,120]
-plot(τ_list,Attribute_list1,label="inference")
-plot!(τ_list,Attribute_list2,label="theory")
-scatter!(τ_list,Attribute_list1,label=:false)
-scatter!(τ_list,Attribute_list2,label=:false)
-
+solution
+sum(set_one(solution))
