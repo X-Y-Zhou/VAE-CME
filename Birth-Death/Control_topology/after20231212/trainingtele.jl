1584519961bb0501cac1980f08b2a9a8455b2079
X-Y-Zhou <2827644213@qq.com>
2023-12-19 22:26:43 +0800

    update

--- Birth-Death/Control_topology/after20231212/trainingtele.jl
+++ Birth-Death/Control_topology/after20231212/trainingtele.jl
@@ -42,7 +42,7 @@ P_0_distribution = Poisson(rho_on*τ*sigma_on)
 P_0 = [pdf(P_0_distribution,j) for j=0:N-1]
 P_0_split = [P_0*sigma_on/(sigma_on+sigma_off);P_0*sigma_off/(sigma_on+sigma_off)]
 
-sol(p,sigma_on,sigma_off,rho_on,P_0) = nlsolve(x->f1!(x,p,sigma_on,sigma_off,rho_on),P_0).zero
+solving(p,sigma_on,sigma_off,rho_on,P_0) = nlsolve(x->f1!(x,p,sigma_on,sigma_off,rho_on),P_0).zero
 solution = sol(p1,sigma_on,sigma_off,rho_on,P_0_split)
 solution = solution[1:N]+solution[N+1:2*N]
 
@@ -101,14 +101,7 @@ NN1_list = []
 NN2_list = []
 x_list = []
 
-solution = sol(p1,sigma_on,sigma_off,rho_on,P_0_split)
-sum(solution[1:N])
-sum(solution[N+1:end])
-3/7
-4/7
-
-plot(0:2N-1,solution)
-plot!(0:2N-1,x_list[end])
+solution = solving(p1,sigma_on,sigma_off,rho_on,P_0_split)
 solution = solution[1:N]+solution[N+1:2*N]
 mse = Flux.mse(solution,train_sol)
 
@@ -124,143 +117,73 @@ plot(x_list[end][1:N]+x_list[end][N+1:2*N])
 x_temp = x_list[end][1:N]+x_list[end][N+1:2*N]
 plot([x_temp*sigma_on/(sigma_on+sigma_off);x_temp*sigma_off/(sigma_on+sigma_off)])
 
-NN1_list
+NN1_list[end]
 plot(NN1_list[1:10])
 plot(NN2_list[1:10])
 NN1_list[end]
 
-function plot_distribution(set)
-    plot(0:N-1,solution[set],linewidth = 3,label="VAE-CME",xlabel = "# of products \n", ylabel = "\n Probability")
-    plot!(0:N-1,train_sol[set],linewidth = 3,label="exact",title=join(["a,b,τ=",ab_list[set]]),line=:dash)
-end
-plot_distribution(1)
+P_split = [P0;P1]
+N
+P_split = x_list[end]
 
-function plot_all()
-    p1 = plot_distribution(1)
-    p2 = plot_distribution(2)
-    p3 = plot_distribution(3)
-    p4 = plot_distribution(4)
-    plot(p1,p2,p3,p4,size=(600,600),layout=(2,2))
-end
-plot_all()
-# # savefig("Control_rate_Inference/control_kinetic/fitting.svg")
+[NN2_list[end];NN1_list[end]]
 
-function plot_all()
-    p1 = plot_distribution(1)
-    p2 = plot_distribution(2)
-    p3 = plot_distribution(3)
-    p4 = plot_distribution(4)
-    p5 = plot_distribution(5)
-    p6 = plot_distribution(6)
-    p7 = plot_distribution(7)
-    p8 = plot_distribution(8)
-    p9 = plot_distribution(9)
-    p10 = plot_distribution(10)
-    plot(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,size=(1500,600),layout=(2,5))
-end
-plot_all()
+NN1_list[end]
+NN2_list[end]
+x_list[end]
 
-a_list = [0.04,0.1]
-b_list = [2.,3.]
-
-
-a_list_pre = [0.04,0.06,0.08,0.1]
-b_list_pre = [2,2.2,2.4,2.6,2.8,3]
-l_ablist_pre = length(a_list_pre)*length(b_list_pre)
-
-ab_list_pre = [[a_list_pre[i],b_list_pre[j]] for i=1:length(a_list_pre) for j=1:length(b_list_pre)]
-
-solution_list = []
-for i=1:l_ablist_pre
-    print(i,"\n")
-    a = ab_list_pre[i][1]
-    b = ab_list_pre[i][2]
-    P_0_distribution = NegativeBinomial(a*τ, 1/(1+b));
-    P_0 = [pdf(P_0_distribution,j) for j=0:N-1]
-
-    ϵ = zeros(latent_size)
-    solution = sol(p1,a,b,P_0)
-    push!(solution_list,solution)
+function f(NN)
+    NN1 = NN[1:N-1]
+    NN2 = NN[N:2*N-2]
+    [(-sigma_on-rho_off)*P_split[1] + (-gamma+NN1[1])*P_split[2] + sigma_off*P_split[N+1];
+    [rho_off*P_split[i-1] + (-sigma_on-rho_off+(i-1)*gamma-NN1[i-1])*P_split[i] + (-i*gamma+NN1[i])*P_split[i+1] + sigma_off*P_split[i+N] for i in 2:N-1];
+    rho_off*P_split[N-1] + (-sigma_on-rho_off+N*gamma-NN1[N-1])*P_split[N] + sigma_off*P_split[2*N];
+    
+    sigma_on*P_split[1] + (-sigma_off-rho_on)*P_split[N+1] + (-gamma+NN2[1])*P_split[N+2];
+    [sigma_on*P_split[i-N] + rho_on*P_split[i-1] + (-sigma_off-rho_on+(i-N-1)*gamma -NN2[i-N-1])*P_split[i] + (-(i-N)*gamma+NN2[i-N])*P_split[i+1] for i in (N+2):(2*N-1)]]
 end
 
-function  plot_distribution(set)
-    p=plot(0:N-1,solution_list[set],linewidth = 3,label="VAE-CME",xlabel = "# of products", ylabel = "\n Probability")
-    plot!(0:N-1,bursty(N,ab_list_pre[set][1],ab_list_pre[set][2],τ),linewidth = 3,label="exact",line=:dash,title=join(["ab=",ab_list_pre[set]]))
+sol = nlsolve(f, [1.;zeros(2*N-2)])
+NN_solution = sol.zero
+NN_solution = NN_solution[1:N-1]+NN_solution[N:2*N-2]
+plot(NN_solution)
+
+f([NN2_list[end];NN1_list[end]])
+sum(f([NN2_list[end];NN1_list[end]]))
+
+
+P_split = x_list[end]
+P_split = [P0;P1]
+function f1!(NN,sigma_on,sigma_off,rho_on)
+    NN1 = NN[1:N-1]
+    NN2 = NN[N:2*N-2]
+    return vcat((-sigma_on-rho_off)*P_split[1] + (-gamma+NN1[1])*P_split[2] + sigma_off*P_split[N+1],
+                [rho_off*P_split[i-1] + (-sigma_on-rho_off+(i-1)*gamma-NN1[i-1])*P_split[i] + (-i*gamma+NN1[i])*P_split[i+1] + sigma_off*P_split[i+N] for i in 2:N-1],
+                rho_off*P_split[N-1] + (-sigma_on-rho_off+N*gamma-NN1[N-1])*P_split[N] + sigma_off*P_split[2*N],
+                
+                sigma_on*P_split[1] + (-sigma_off-rho_on)*P_split[N+1] + (-gamma+NN2[1])*P_split[N+2],
+                [sigma_on*P_split[i-N] + rho_on*P_split[i-1] + (-sigma_off-rho_on+(i-N-1)*gamma -NN2[i-N-1])*P_split[i] + (-(i-N)*gamma+NN2[i-N])*P_split[i+1] for i in (N+2):(2*N-1)],
+                )
 end
 
-function plot_all()
-    p1 = plot_distribution(1)
-    p2 = plot_distribution(2)
-    p3 = plot_distribution(3)
-    p4 = plot_distribution(4)
-    p5 = plot_distribution(5)
-    p6 = plot_distribution(6)
-    p7 = plot_distribution(7)
-    p8 = plot_distribution(8)
-    p9 = plot_distribution(9)
-    p10 = plot_distribution(10)
-    p11 = plot_distribution(11)
-    p12 = plot_distribution(12)
-    p13 = plot_distribution(13)
-    p14 = plot_distribution(14)
-    p15 = plot_distribution(15)
-    p16 = plot_distribution(16)
-    p17 = plot_distribution(17)
-    p18 = plot_distribution(18)
-    p19 = plot_distribution(19)
-    p20 = plot_distribution(20)
-    p21 = plot_distribution(21)
-    p22 = plot_distribution(22)
-    p23 = plot_distribution(23)
-    p24 = plot_distribution(24)
-    plot(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,
-         p16,p17,p18,p19,p20,p21,p22,p23,p24,size=(1800,1200),layout=(4,6))
+solving(sigma_on,sigma_off,rho_on,P_0) = nlsolve(NN->f1!(NN,sigma_on,sigma_off,rho_on),P_0).zero
+solution = solving(sigma_on,sigma_off,rho_on,P_0_split)
+solution = solution[1:N]+solution[N+1:2*N]
+plot(solution)
+
+
+
+train_sol = bursty(N,a,b,τ)
+a = 0.0282
+b = 3.46
+function f(NN)
+    [-a*b/(1+b)*train_sol[1]+NN[1]*train_sol[2];
+    [sum(a*(b/(1+b))^(i-j)/(1+b)*train_sol[j] for j in 1:i-1) - 
+    (a*b/(1+b)+NN[i-1])*train_sol[i] + NN[i]*train_sol[i+1] for i in 2:N-1];0]
 end
-plot_all()
-savefig("Control_rate_Inference/control_kinetic/predicting.pdf")
 
-3.0/((1.0+2.0)*(1.0+2.0))
-
-sigma_on = 0.0282
-sigma_off = 1
-rho_on = 3.46
-w1(u) = (-(sigma_on+sigma_off+rho_on*(1-u))-sqrt((sigma_on+sigma_off+rho_on*(1-u))^2-4*sigma_on*rho_on*(1-u)))/2
-w2(u) = (-(sigma_on+sigma_off+rho_on*(1-u))+sqrt((sigma_on+sigma_off+rho_on*(1-u))^2-4*sigma_on*rho_on*(1-u)))/2
-G(u) = (w2(u)*exp(w1(u))-w1(u)*exp(w2(u)))/(w2(u)-w1(u))+rho_on*sigma_on*(u-1)*(exp(w2(u))-exp(w1(u)))/((sigma_off+sigma_on)*(w2(u)-w1(u)))
-
-taylorexpand = taylor_expand(x->G(x),0,order=N)
-P = zeros(N)
-for j in 1:N
-    P[j] = taylorexpand[j-1]
-end
-P
-plot(P)
-
-using Combinatorics
-factorial(5,3)
-factorial(5)
-
-k1 = sigma_off-sigma_on + 2*im*sqrt(sigma_off*sigma_on)
-k2 = sigma_off-sigma_on - 2*im*sqrt(sigma_off*sigma_on)
-
-l = 3
-i = 21
-s = 1
-
-# M(s,i) = 
-
-
-l = 3
-i = 1
-s = 1
-[sum([round(binomial(l,w)*binomial(l,i-w)*(-1)^i*factorial(big(i))/
-factorial(big(2*l+s))*((rho_on/2+k1/2)^(l-w))*((rho_on/2+k2/2)^(l-i+w)),digits=2) for w=maximum([0,i-l]):minimum([l,i])]) 
-for l=Int(ceil(i/2)):20]
-
-sum([round(binomial(l,w)*binomial(l,i-w)*(-1)^i*factorial(big(i))/factorial(big(2*l+s))*
-((rho_on/2+k1/2)^(l-w))*((rho_on/2+k2/2)^(l-i+w)),digits=10) for w=maximum([0,i-l]):minimum([l,i])])
-
-real( (1 + 2im) + 0.5)
-
-
-
+sol = nlsolve(f, [2.;zeros(N-1)])
+NN_solution = sol.zero
+plot(NN_solution)
+NN_solution = NN_bursty(N,a,b,τ)
+plot!(NN_solution)
--- Birth-Death/Control_topology/after20231218/training_bursty_Poisson.jl
+++ Birth-Death/Control_topology/after20231218/training_bursty_Poisson.jl
@@ -68,7 +68,7 @@ lr = 0.01;  #lr需要操作一下的
 
 # for lr in lr_list
 using CSV,DataFrames
-df = CSV.read("Birth-Death/Control_topology/after20231212-2/params_trained_bp.csv",DataFrame)
+df = CSV.read("Birth-Death/Control_topology/after20231218/params_trained_Poisson.csv",DataFrame)
 p1 = df.p1
 ps = Flux.params(p1);
 
@@ -87,7 +87,7 @@ mse_list = []
     mse = loss_func(p1)
     if mse<mse_min[1]
         df = DataFrame(p1 = p1)
-        CSV.write("Birth-Death/Control_topology/after20231212-2/params_trained_bp.csv",df)
+        CSV.write("Birth-Death/Control_topology/after20231218/params_trained_Poisson.csv",df)
         mse_min[1] = mse
     end
     
@@ -100,7 +100,7 @@ mse_min = [9.261976021772277e-6]
 mse_min 
 
 using CSV,DataFrames
-df = CSV.read("Birth-Death/Control_topology/after20231212-2/params_trained_bp.csv",DataFrame)
+df = CSV.read("Birth-Death/Control_topology/after20231218/params_trained_Poisson.csv",DataFrame)
 p1 = df.p1
 ps = Flux.params(p1);
 
@@ -109,7 +109,7 @@ mse = sum(Flux.mse(solution[i],train_sol[i]) for i=1:l_ablist)/l_ablist
 
 function plot_distribution(set)
     plot(0:N-1,solution[set],linewidth = 3,label="VAE-CME",xlabel = "# of products \n", ylabel = "\n Probability")
-    plot!(0:N-1,train_sol[set],linewidth = 3,label="exact",title=join(["a,b,τ=",ab_list[set]]),line=:dash)
+    plot!(0:N-1,train_sol[set],linewidth = 3,label="exact",title=join(["a,b=",ab_list[set]]),line=:dash)
 end
 plot_distribution(1)
 
@@ -127,58 +127,3 @@ function plot_all()
     plot(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,size=(1500,600),layout=(2,5))
 end
 plot_all()
-
-a_list_pre = [0.04,0.06,0.08,0.1]
-b_list_pre = [2,2.2,2.4,2.6,2.8,3]
-l_ablist_pre = length(a_list_pre)*length(b_list_pre)
-
-ab_list_pre = [[a_list_pre[i],b_list_pre[j]] for i=1:length(a_list_pre) for j=1:length(b_list_pre)]
-
-solution_list = []
-for i=1:l_ablist_pre
-    print(i,"\n")
-    a = ab_list_pre[i][1]
-    b = ab_list_pre[i][2]
-    P_0_distribution = NegativeBinomial(a*τ, 1/(1+b));
-    P_0 = [pdf(P_0_distribution,j) for j=0:N-1]
-
-    ϵ = zeros(latent_size)
-    solution = sol(p1,a,b,P_0)
-    push!(solution_list,solution)
-end
-
-function  plot_distribution(set)
-    p=plot(0:N-1,solution_list[set],linewidth = 3,label="VAE-CME",xlabel = "# of products", ylabel = "\n Probability")
-    plot!(0:N-1,bursty(N,ab_list_pre[set][1],ab_list_pre[set][2],τ),linewidth = 3,label="exact",line=:dash,title=join(["ab=",ab_list_pre[set]]))
-end
-
-function plot_all()
-    p1 = plot_distribution(1)
-    p2 = plot_distribution(2)
-    p3 = plot_distribution(3)
-    p4 = plot_distribution(4)
-    p5 = plot_distribution(5)
-    p6 = plot_distribution(6)
-    p7 = plot_distribution(7)
-    p8 = plot_distribution(8)
-    p9 = plot_distribution(9)
-    p10 = plot_distribution(10)
-    p11 = plot_distribution(11)
-    p12 = plot_distribution(12)
-    p13 = plot_distribution(13)
-    p14 = plot_distribution(14)
-    p15 = plot_distribution(15)
-    p16 = plot_distribution(16)
-    p17 = plot_distribution(17)
-    p18 = plot_distribution(18)
-    p19 = plot_distribution(19)
-    p20 = plot_distribution(20)
-    p21 = plot_distribution(21)
-    p22 = plot_distribution(22)
-    p23 = plot_distribution(23)
-    p24 = plot_distribution(24)
-    plot(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,
-         p16,p17,p18,p19,p20,p21,p22,p23,p24,size=(1800,1200),layout=(4,6))
-end
-plot_all()
-savefig("Control_rate_Inference/control_kinetic/predicting.pdf")
