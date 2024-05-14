ede8a1ec6f7ef20bc059b36a3f44e9edda1edc58
X-Y-Zhou <2827644213@qq.com>
2024-05-14 13:51:58 +0800

    update

--- Topologyv3/SSA_tele.jl
+++ Topologyv3/SSA_tele.jl
@@ -143,3 +143,42 @@ writedlm("Topologyv3/tele/data/matrix_tele_$T1-$T2.csv",matrix_tele)
 # plot(matrix_tele[:,3],lw=3)
 # plot!(P3,lw=3,line=:dash)
 
+T1T2_list = [[0,200],[100,100]]
+
+matrix_tele_list = []
+for j = 1:2
+    T1 = T1T2_list[j][1]
+    T2 = T1T2_list[j][2]
+    matrix_tele = readdlm("Topologyv3/tele/data/matrix_tele_$T1-$T2.csv")
+    push!(matrix_tele_list,matrix_tele)
+end
+matrix_tele_list
+
+function plot_distribution(set)
+    plot(0:N-1,matrix_tele_list[1][:,set],linewidth = 3,line=:dash,title=round.(ps_matrix[:,set],digits=4))
+    plot!(0:N-1,matrix_tele_list[2][:,set],linewidth = 3,line=:dash)
+    # plot!(0:N-1,matrix_tele_list[3][:,set],linewidth = 3,line=:dash)
+    # plot!(0:N-1,matrix_tele_list[4][:,set],linewidth = 3,line=:dash)
+    # plot!(0:N-1,matrix_tele_list[5][:,set],linewidth = 3,line=:dash)
+end
+plot_distribution(50)
+
+function plot_channel(i)
+    p1 = plot_distribution(1+10*(i-1))
+    p2 = plot_distribution(2+10*(i-1))
+    p3 = plot_distribution(3+10*(i-1))
+    p4 = plot_distribution(4+10*(i-1))
+    p5 = plot_distribution(5+10*(i-1))
+    p6 = plot_distribution(6+10*(i-1))
+    p7 = plot_distribution(7+10*(i-1))
+    p8 = plot_distribution(8+10*(i-1))
+    p9 = plot_distribution(9+10*(i-1))
+    p10 = plot_distribution(10+10*(i-1))
+    plot(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,layouts=(2,5),size=(1500,600))
+end
+plot_channel(5)
+
+for i = 1:5
+    p = plot_channel(i)
+    savefig(p,"Topologyv3/tele/data/compare/fig_$i.svg")
+end
